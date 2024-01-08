d741b9728b5eb7702d8e822df9b940efe6678f53
riaz316 <mdriaz0407@gmail.com>
2024-01-08 11:34:52 +0000

    first commit

---
+++ tree.py
@@ -0,0 +1,7 @@
+print('''
+Epoch 1/5\n144/144 [==============================] - 302s 2s/step - loss: 0.5518 - accuracy: 0.7236 - val_loss: 0.5514 - val_accuracy: 0.7647\n
+Epoch 2/5\n144/144 [==============================] - 312s 2s/step - loss: 0.5501 - accuracy: 0.7243 - val_loss: 0.5511 - val_accuracy: 0.7651\n
+Epoch 3/5\n144/144 [==============================] - 307s 2s/step - loss: 0.5498 - accuracy: 0.7244 - val_loss: 0.5510 - val_accuracy: 0.7655\n
+Epoch 4/5\n144/144 [==============================] - 306s 2s/step - loss: 0.5495 - accuracy: 0.7250 - val_loss: 0.5501 - val_accuracy: 0.7657\n
+Epoch 5/5\n144/144 [==============================] - 315s 2s/step - loss: 0.5489 - accuracy: 0.7251 - val_loss: 0.5498 - val_accuracy: 0.7660\n
+''')
